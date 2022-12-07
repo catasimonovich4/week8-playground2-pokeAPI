387fce939307444cb2e64cc8a95da707af77ec63
Catalina Simonovich <catalinasimonovich1@gmail.com>
2022-12-07 16:15:56 -0300

    :sparkles:Feat: Generate first 20 Pokemon cards with JS

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-// API request and conection 
-const API_URL = "https://pokeapi.co/api/v2/pokemon/"
+// API request and connection 
+const API_URL = "https://pokeapi.co/api/v2/pokemon/" 
 
 const getPokeAPI = async() => {
     try {
+        //---- Connection with API URL
         const getFetch = await fetch(API_URL);
         const getData = await getFetch.json();
         console.log(getData);
         const getPokemons = getData.results
         console.log(getPokemons)
-        setCards(getPokemons);
         
-        console.log(getData.results.url)
-        const getPokemonsFetch = await fetch(getData.results.url);
-        const getPokemonData = await getPokemonsFetch.json();
-        console.log(getPokemonData)
+        //---- Get each Pokemon data
+        for(let i=0; i < getPokemons.length; i++) {
+            fetch(getData.results[i].url)
+            .then(resp => resp.json())
+            .then(pokes => 
+                setCards(pokes),
+                //console.log(pokes)
+            )
+            
+        }
+
     }
     catch(error) {
         console.log("Error: ", error.message);
@@ -22,41 +29,60 @@ const getPokeAPI = async() => {
 getPokeAPI();
 
 
+// Get 20 more Pokemons 
+const getMorePoke = async() => {
+    try {
+        //---- Connection with API URL
+        const getFetch = await fetch(API_URL);
+        const getData = await getFetch.json();
+        //console.log(getData);
+
+        //---- Connection with NEXT URL
+        const morePoke = getData.next
+        //console.log(morePoke) 
+        const getMorePokeFetch = await fetch(morePoke);
+        const getMorePokeData = await getMorePokeFetch.json();
+        //console.log(getMorePokeData)
+        const getMorePoke = getMorePokeData.results
+
+        //---- Get each Pokemon data
+        for(let i=0; i < getMorePoke.length; i++) {
+            fetch(getMorePokeData.results[i].url)
+            .then(resp => resp.json())
+            .then(pokes => 
+                setCards(pokes),
+                //console.log(pokes)
+            )
+            
+        }
+
+        
+
+        
+    } catch(error) {
+        console.log("Error: ", error.message);
+    }
+}
+
+
 
 // Rendering cards
 function setCards(pokeArray) {
     //console.log(pokeArray)
     const pokeCards = document.getElementById("cards-container")
-    for(let i=0; i<pokeArray.length; i++) {
-        const divPokeCards = document.createElement("div");
-        divPokeCards.classList.add("pokeCards")
-        divPokeCards.innerHTML = `
+    const divPokeCards = document.createElement("div");
+    divPokeCards.classList.add("pokeCards")
+    divPokeCards.innerHTML = `
         <div class="card" style="width: 18rem;">
-            <img src="" class="card-img-top" alt="">
+            <img src="${pokeArray.sprites.front_default}" class="card-img-top" alt="">
             <div class="card-body">
-                <h5 class="card-title">${pokeArray[i].name.toUpperCase()}</h5>
+                <h5 class="card-title">${pokeArray.name.toUpperCase()}</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
         </div>
-        `
-        pokeCards.classList.add("cards")
-        pokeCards.appendChild(divPokeCards);
-    }
-
-   /*  pokeArray.forEach(pokemon => {
-        const divPokeCards = document.createElement("div");
-        divPokeCards.classList.add("pokeCards")
-        divPokeCards.innerHTML = `
-        <div class="card" style="width: 18rem;">
-            <img src="" class="card-img-top" alt="">
-            <div class="card-body">
-                <h5>${pokemon.results.name}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-        `
-        pokeCards.appendChild(divPokeCards);
-    }); */
+    `
+    pokeCards.classList.add("cards")
+    pokeCards.appendChild(divPokeCards);
 }
 
 
@@ -81,4 +107,4 @@ function search() {
   
     });
   
-  }
+}
